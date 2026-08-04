cdba357642d12aa3f4a9dbe3add9b8992d292cfc
MatiasMedina846 <matimedina111@gmail.com>
2026-08-04 18:43:21 -0300

    feature: spring backend

---
+++ SistemaDeGestion/src/main/java/com/example/SistemaDeGestion/SistemaDeGestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionApplication.class, args);
+	}
+
+}
---
+++ SistemaDeGestion/src/test/java/com/example/SistemaDeGestion/SistemaDeGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeGestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
